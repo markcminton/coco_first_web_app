fd65726ee3804e1fefe74e37f0d85e418b5171a8
Dsmash 97 <markcminton@gmail.com>
2022-02-21 21:19:50 -0600

    portable path in js

--- js/app.js
+++ js/app.js
@@ -6,17 +6,17 @@ function coco(cocoID)
     console.log(cocoID);
     if (cocoID == 1)
     {
-        audio = new Audio('/sounds/chii.mp3');
+        audio = new Audio('sounds/chii.mp3');
         audio.play();
     }
     else if(cocoID == 2)
     {
-        audio = new Audio('/sounds/bii.mp3');
+        audio = new Audio('sounds/bii.mp3');
         audio.play();
     }
     else
     {
-        audio = new Audio('/sounds/mmm.mp3');
+        audio = new Audio('sounds/mmm.mp3');
         audio.play();
     }
 
